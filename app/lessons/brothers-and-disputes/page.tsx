20cfd8e4d78eb6d754a4ab4c9aa32cb1e0c7d195
Baron17971 <159436565+Baron17971@users.noreply.github.com>
2026-09-15 01:26:04 +0300

    Fix disputes lesson route index

--- app/lessons/brothers-and-disputes/page.tsx
+++ app/lessons/brothers-and-disputes/page.tsx
@@ -1,3 +1,3 @@
 import type { Metadata } from "next";import { LessonView } from "../../lesson-view";import { lessons } from "../../data";
-export const metadata:Metadata={title:`${lessons[3].title} | אני ישראלי`,description:lessons[3].hook};
-export default function Page(){return <LessonView index={3}/>}
+export const metadata:Metadata={title:`${lessons[4].title} | אני ישראלי`,description:lessons[4].hook};
+export default function Page(){return <LessonView index={4}/>}
