245e61c05b45b330f4557546e95ef20e12c5c9a1
Baron17971 <159436565+Baron17971@users.noreply.github.com>
2026-09-15 12:23:36 +0300

    Unify design system details

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./design-system.css";
 import { HomeThemeShell } from "./home-theme-shell";
 
 export const metadata: Metadata = {
